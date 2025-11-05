8927398c71a6095a9fa10d17a6b20fd1270796f8
Carlos Valeriano <carlos.valeriano@tecsup.edu.pe>
2025-11-05 15:40:13 -0500

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,9 +8,7 @@ plugins {
 
 android {
     namespace = "com.tecsup.metrolimago"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.tecsup.metrolimago"
